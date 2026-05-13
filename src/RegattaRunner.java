3e93b4de360e99be1fdc4acdaf76490955598039
Henry Van Kirk <henryvankirk28@jpiicardinals.org>
2026-05-13 15:11:21 -0500

    Add JFrame setup to RegattaRunner

--- src/RegattaRunner.java
+++ src/RegattaRunner.java
@@ -1,5 +1,13 @@
+import javax.swing.JFrame;
+
 public class RegattaRunner {
     public static void main(String[] args) {
         RegattaList regattaList = new RegattaList();
+        JFrame myFrame = new JFrame("Regatta Runner");
+        myFrame.add(new HighDensityChip());
+        myFrame.setSize(1300, 700);
+        myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        myFrame.setLocationRelativeTo(null); 
+        myFrame.setVisible(true);  
     }
 }
